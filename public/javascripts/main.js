b324b6a1ad4ad2366edc330a33c7d252dfdd462a
Zaman Harris <zaman.harris@cyclingsportsgroup.com>
2022-03-01 10:48:49 -0500

    update url parameter name

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,11 @@
 const queryString = window.location.search
 const urlParams = new URLSearchParams(queryString);
-const serial = urlParams.get('serial')
+const serial = urlParams.get('serial_number')
 
 let registerUrl = document.getElementById('registerUrl').href
 
 const redirectUrl = new URL(registerUrl)
 
-redirectUrl.searchParams.set('serial', serial)
+redirectUrl.searchParams.set('serial_number', serial)
 
 document.getElementById('registerUrl').href = redirectUrl.toString()
