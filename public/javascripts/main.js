5c05f76077005c082b48452899c2a2df097b7449
Zaman Harris <zaman.harris@cyclingsportsgroup.com>
2022-03-01 12:13:12 -0500

    commenting out the javascript redirect

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -10,4 +10,4 @@ redirectUrl.searchParams.set('serial_number', serial)
 
 document.getElementById('registerUrl').href = redirectUrl.toString()
 
-setTimeout(() => {window.location.replace(redirectUrl.toString())}, 100)
+// setTimeout(() => {window.location.replace(redirectUrl.toString())}, 100)
